d35aaed1f5e179f798d932ecb3ca1742f54f42b7
Andrei Stănimir <andreistanimir@live.com>
2023-11-26 19:53:10 +0200

    add dbcontext

---
+++ DBContext.cs
@@ -0,0 +1,53 @@
+﻿using SQLite;
+using System.Text.Json;
+
+public class DatabaseContext
+{
+    private readonly SQLiteAsyncConnection Database;
+
+    public DatabaseContext(string dbPath)
+    {
+        Database = new SQLiteAsyncConnection(dbPath);
+        Database.CreateTableAsync<MajorCity>().Wait();
+        Database.CreateTableAsync<CityInfo>().Wait();
+    }
+
+    public Task<List<CityInfo>> GetCitiesAsync()
+    {
+        return Database.Table<CityInfo>().ToListAsync();
+    }
+
+    public Task<List<MajorCity>> GetMajorCitiesAsync()
+    {
+        return Database.Table<MajorCity>().ToListAsync();
+    }
+
+    public Task<CityInfo> GetCityInfoByMajorCityIdAsync(int majorCityId)
+    {
+        return Database.Table<CityInfo>()
+                        .Where(ci => ci.MajorCityId == majorCityId)
+                        .FirstOrDefaultAsync();
+    }
+    public async Task InsertCitiesFromJsonAsync(string cityInfoJsonPath)
+    {
+        string cityInfoJson = File.ReadAllText(cityInfoJsonPath);
+        var cities = JsonSerializer.Deserialize<List<CityInfo>>(cityInfoJson);
+
+        if (cities != null)
+        {
+            await Database.InsertAllAsync(cities);
+        }
+    }
+
+    public async Task InsertMajorCitiesFromJsonAsync(string majorCityJsonPath)
+    {
+        string majorCityJson = File.ReadAllText(majorCityJsonPath);
+        var majorCities = JsonSerializer.Deserialize<List<MajorCity>>(majorCityJson);
+
+        if (majorCities != null)
+        {
+            await Database.InsertAllAsync(majorCities);
+        }
+    }
+    // Add methods to insert, update, delete records
+}
